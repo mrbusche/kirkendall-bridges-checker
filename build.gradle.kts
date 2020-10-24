78bbe83984442cfe007c2808a3fa04c6d5053b44
Scott Busche <swbusche@gmail.com>
2020-10-24 13:25:46 -0500

    Swap to Kotlin Conventions and Tests (#7)
    * Convert code to more Kotlin style
    
    * Convert to Kotlin JUnit
    
    * Cleanup gradle dependencies further

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("org.springframework.boot") version "2.4.0-M4"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("java")
-    id("groovy")
     id("org.sonarqube") version "3.0"
     id("org.jetbrains.kotlin.jvm") version "1.4.10"
 }
@@ -16,25 +17,22 @@ repositories {
     maven { url = uri("https://repo.spring.io/milestone") }
 }
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web") {
-        exclude("com.fasterxml.jackson.core")
-        exclude("com.fasterxml.jackson.datatype")
-        exclude("com.fasterxml.jackson.module")
-    }
     implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude("org.junit.vintage", "junit-vintage-engine")
-    }
-    implementation("org.jsoup:jsoup:1.13.1")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-    implementation("org.apache.commons:commons-lang3")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.vintage:junit-vintage-engine")
-    testImplementation("org.spockframework:spock-core:2.0-M3-groovy-3.0")
-    implementation("org.codehaus.groovy:groovy:3.0.5")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.jsoup:jsoup:1.13.1")
 }
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = FULL
+    }
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
