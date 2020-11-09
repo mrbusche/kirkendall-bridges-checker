61f466dc68f0fed33e00259b9b65a4c1370a954f
Matt Busche <mrbusche@gmail.com>
2020-11-08 19:48:02 -0600

    less books

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -10,7 +10,7 @@ internal class CheckerApplicationTests {
     fun `Test New AudioBook Counts`() {
         val newBooks = CheckerService.retrieveNewBookCount(audioBooksURL)
         if (newBooks != 0) {
-            assertEquals(86, newBooks)
+            assertEquals(58, newBooks)
         }
     }
 
@@ -18,7 +18,7 @@ internal class CheckerApplicationTests {
     fun `Test New eBook Counts`() {
         val newBooks = CheckerService.retrieveNewBookCount(ebooksURL)
         if (newBooks != 0) {
-            assertEquals(77, newBooks)
+            assertEquals(57, newBooks)
         }
     }
 }
