554a2c46de7139db884e851932b7eafa2fb53d5f
Matt Busche <mrbusche@gmail.com>
2021-01-26 13:06:34 -0600

    Update CheckerApplicationTests.kt (#75)

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -10,7 +10,7 @@ internal class CheckerApplicationTests {
     fun `Test New AudioBook Counts`() {
         val audiobooks = CheckerService.retrieveNewBookCount(audioBooksURL)
         if (audiobooks != 0) {
-            assertEquals(4, audiobooks)
+            assertEquals(9, audiobooks)
         }
     }
 
@@ -18,7 +18,7 @@ internal class CheckerApplicationTests {
     fun `Test New eBook Counts`() {
         val ebooks = CheckerService.retrieveNewBookCount(ebooksURL)
         if (ebooks != 0) {
-            assertEquals(19, ebooks)
+            assertEquals(38, ebooks)
         }
     }
 }
