c04b40db03e3f44a10a6c1e070aed1a718d87fc7
Matt Busche <mrbusche@gmail.com>
2021-07-10 17:25:38 -0500

    null check

--- src/main/kotlin/com/webpage/checker/CheckerService.kt
+++ src/main/kotlin/com/webpage/checker/CheckerService.kt
@@ -13,7 +13,7 @@ class CheckerService {
             val document = Jsoup.connect(url).get()
             val elements = document.getElementsByClass(SECTION_LIST_CLASS)
             if (elements.first().toString().contains("days-0-7")) {
-                val element = elements.first().attr("class", SECTION_LIST_ITEM_CLASS)
+                val element = elements?.first()?.attr("class", SECTION_LIST_ITEM_CLASS)
                 return element.childNode(3).childNode(0).toString().replace(",","").toInt()
             }
             return 0
