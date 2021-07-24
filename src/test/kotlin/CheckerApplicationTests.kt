4ff01dda4094195226fe349bacd5f62468c15953
Matt Busche <mrbusche@gmail.com>
2021-07-23 21:35:08 -0500

    Update CheckerApplicationTests.kt

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test
 internal class CheckerApplicationTests {
     private val audioBooksURL = "https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479"
     private val ebooksURL = "https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473"
-    private val audioCount = 0
-    private val eBookCount = 5
+    private val audioCount = 4
+    private val eBookCount = 9
 
     @Test
     fun `Test New AudioBook Counts`() {
