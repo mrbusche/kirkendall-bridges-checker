cf2b2b9a0bc248a7925fb2ccd93e50d68053a9a6
Matt Busche <mrbusche@gmail.com>
2021-08-25 10:59:25 -0500

    Update CheckerApplicationTests.kt

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -6,7 +6,7 @@ internal class CheckerApplicationTests {
     private val audioBooksURL = "https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479"
     private val ebooksURL = "https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473"
     private val audioCount = 0
-    private val eBookCount = 7
+    private val eBookCount = 9
 
     @Test
     fun `Test New AudioBook Counts`() {
